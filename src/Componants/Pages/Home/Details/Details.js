cc9f329c5c8a4503c96150f15e2d2770de409764
zonaetmunna <zonaetmonna@gmail.com>
2021-10-18 02:26:47 +0600

    details add

--- src/Componants/Pages/Home/Details/Details.js
+++ src/Componants/Pages/Home/Details/Details.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
+import useDetails from '../../../../Hooks/useDetails';
 
 const Details = () => {
      const { id } = useParams();
+     const [details] = useDetails();
      return (
           <div>
                <h1>{id}</h1>
+               <Container>
+                    <Row>
+                         <Col sm={6} md={6}>
+                              <Card>
+                                   <Card.Img variant="top" src={details.image} />
+                                   <Card.Body>
+                                        <Card.Title>{details.name}</Card.Title>
+                                        <Card.Text>{details.description}</Card.Text>
+                                   </Card.Body>
+
+                              </Card>
+                         </Col>
+                    </Row>
+               </Container>
           </div>
      );
 };
---
+++ src/Hooks/useDetails.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+const useDetails = () => {
+     const [details, setDetails] = useState([])
+     useEffect(() => {
+          fetch('/Health-data.json')
+               .then(res => res.json())
+               .then(data => setDetails(data[0]))
+     }, [])
+
+     return [details];
+}
+export default useDetails;
