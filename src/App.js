b8da4ac956ec3d25289548f577addbf93111b017
zonaetmunna <zonaetmonna@gmail.com>
2021-10-18 01:29:45 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from './Componants/Pages/Home/Home/Home';
+import Footer from './Componants/Shared/Footer';
+import Header from './Componants/Shared/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
 
     </div>
   );
---
+++ src/Componants/Shared/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+     return (
+          <div>
+               <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Container>
+                         <Navbar.Brand href="#home">Medical & Health center</Navbar.Brand>
+                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                         <Navbar.Collapse id="responsive-navbar-nav">
+                              <Nav className="me-auto">
+                                   <Nav.Link href="/home">Home</Nav.Link>
+
+
+                              </Nav>
+                              <Nav className="d-flex justify-content-center algin-items-center">
+                                   <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                                   <Navbar.Text className="ms-3">
+                                        Signed in as:<Link to=""> Mark Otto</Link>
+                                   </Navbar.Text>
+                              </Nav>
+                         </Navbar.Collapse>
+                    </Container>
+               </Navbar>
+          </div >
+     );
+};
+
+export default Header;
