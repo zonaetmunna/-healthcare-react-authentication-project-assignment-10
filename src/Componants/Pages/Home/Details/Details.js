3762db928c462e849ee3d38711ce62e29f3d05a1
zonaetmunna <zonaetmonna@gmail.com>
2021-10-19 18:18:29 +0600

    add

--- src/Componants/Pages/Home/Details/Details.js
+++ src/Componants/Pages/Home/Details/Details.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+
 import { Card, Col, Container, Row, Button } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router';
+import useDetails from '../../../../Hooks/useDetails';
 
 
 
@@ -8,26 +9,20 @@ const Details = () => {
      const { id } = useParams();
      const history = useHistory();
 
-     const [details, setDetails] = useState([])
+     const [details] = useDetails();
 
-     useEffect(() => {
-          fetch('./Health-data.json')
-               .then(res => res.json())
-               .then(data => {
-                    setDetails(data[0])
-               })
 
-     }, [])
      const handleBooking = () => {
           history.push('/booking')
      }
 
      return (
           <div className="mt-5 pt-5">
-               <h1>{id}</h1>
+
 
 
                <Container className="m-5 p-5">
+                    <h1>{id}</h1>
                     <Row>
                          <Col sm={12} md={6}>
                               <Card className="border-0">
